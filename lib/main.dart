67bc56ebaf6db0b940be04eded732773e31de170
teejay <tjayarancinaofficial@gmail.com>
2025-09-14 00:03:27 +0800

    feat(base): add GameCharacter with getters/setters and methods

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,10 @@
+import 'game_character.dart';
 void main(){
-  
+  print('--- Demo base class GameCharacter ---');
+  GameCharacter char1 = GameCharacter(name: 'char1', role: 'assasin');
+  GameCharacter char2 = GameCharacter(name: 'char2', role: 'mage');
+  char1.getInfo(); //method that access properties
+  print(char1.level); //getter
+  char1.level = 2; //setter w/ validation
+  char1.attack(char2, 1000);//method2
 }
